e22fbf89a4cc1385ac77cd7e96582b36e0823821
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2022-09-26 14:15:03 +0200

    [rm] log envelope

--- src/ipc.rs
+++ src/ipc.rs
@@ -6,6 +6,12 @@ use std::fmt::{Display, Formatter};
 use std::io::{Result, Write};
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct RunnerMetadata {
+    pub slurm_job: slurm::JobId,
+    pub runner_name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct LogEntry {
     level: Level,
     message: String,
@@ -19,6 +25,12 @@ impl LogEntry {
             None => log!(self.level, "{}", self.message),
         }
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Envelope {
+    Metadata(RunnerMetadata),
+    Log(LogEntry),
 }
 
 pub struct LogFormatter {
@@ -35,33 +47,21 @@ impl LogFormatter {
     }
 
     pub fn format<W: Write>(self: &Self, w: &mut W, record: &Record) -> Result<()> {
-        let envelope = LogEntry {
+        let envelope = Envelope::Log(LogEntry {
             level: record.metadata().level(),
             message: record.args().to_string(),
             job: self.job,
-        };
+        });
         serde_json::to_writer(w as &mut _, &envelope)?;
         w.write(b"\n")?;
         Ok(())
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct RunnerMetadata {
-    pub slurm_job: slurm::JobId,
-    pub runner_name: String,
-}
-
 impl Display for RunnerMetadata {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} (SLURM #{})", self.runner_name, self.slurm_job)
     }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub enum Envelope {
-    Metadata(RunnerMetadata),
-    Log(LogEntry),
 }
 
 pub fn parse(json: &str) -> anyhow::Result<Envelope> {
